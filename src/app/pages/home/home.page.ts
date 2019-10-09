7da3257ed7671f1caaa73699932543ea6ebc65dd
Adelys <adalbeca@gmail.com>
2019-10-09 10:06:42 -0300

    add calendar

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+    maxDate: Date = new Date();
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+      console.log(this.maxDate.toISOString());
+    }
+
+    getDate(event) {
+        console.log(event.detail.value);
+    }
+    getInput(event){
+      console.log(event.detail.value);
+    }
 
 }
