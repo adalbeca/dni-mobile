35820e7e94edbdd588a7e5caa73e1cc49725acfe
Adelys Belen <adalbeca@gmail.com>
2019-10-09 23:18:23 -0300

    add post method

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
+import {DataService} from '../../services/data.service';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -6,21 +10,49 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+    exp: string;
+    dia: string;
+    mes: string;
+    anio: string;
     maxDate: Date = new Date();
+    customPickerOptions;
+    myData: object;
 
-    constructor() {
+    constructor(private router: Router, private dataService: DataService ) {
     }
 
     ngOnInit() {
-      console.log(this.maxDate.toISOString());
+        this.customPickerOptions = {
+            buttons: [{
+                text: 'Ok',
+                handler: (evento) => console.log('Clicked Save!', evento)
+            }, {
+                text: 'Cancelar',
+                handler: () => {
+                    console.log('Clicked Log. Do not Dismiss.');
+                    return true;
+                }
+            }]
+        };
+        console.log(this.maxDate.toISOString());
     }
 
     getDate(event) {
-        console.log(event.detail.value);
+        const date = moment(event.detail.value);
+        this.dia = date.format('DD');
+        this.mes = date.format('MM');
+        this.anio = date.format('YYYY');
     }
-    getInput(event){
-      console.log(event.detail.value);
+
+    getInput(event) {
+        this.exp = event.detail.value;
+    }
+
+    getFromData() {
+        const data = {exp: this.exp, dia: this.dia, mes: this.mes, anio: this.anio};
+        this.dataService.getData(data)
+            .subscribe((value) => this.myData = value);
+        this.router.navigate(['detail'], this.myData);
     }
 
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(data) {
+    console.log(data);
+    //const data = {exp: '1410852017', dia: '05', mes: '05', anio: '1983'};
+    return this.http.post('https://api-dni.adalbeca.com/giveme', data);
+  }
+}
